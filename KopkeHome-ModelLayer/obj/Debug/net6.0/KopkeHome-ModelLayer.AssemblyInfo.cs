ef03fb14b9ebe1a3bea8848658cfd0589eafb505
perryneal11 <perryneal11@gmail.com>
2024-03-02 22:12:07 -0500

    sdF

--- KopkeHome-ModelLayer/obj/Debug/net6.0/KopkeHome-ModelLayer.AssemblyInfo.cs
+++ KopkeHome-ModelLayer/obj/Debug/net6.0/KopkeHome-ModelLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KopkeHome-ModelLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21221edbb696d2f267e3ba0a17c180fa7dfe2bdc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87ea0774f27be60e0ec20eff73aacca445501d12")]
 [assembly: System.Reflection.AssemblyProductAttribute("KopkeHome-ModelLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KopkeHome-ModelLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
