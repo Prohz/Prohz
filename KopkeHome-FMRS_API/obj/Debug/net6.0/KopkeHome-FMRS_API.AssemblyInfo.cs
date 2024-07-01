5b1a27a398b6e9c8759f1534563356b25c037bbe
perryneal11 <perryneal11@gmail.com>
2024-07-01 11:21:52 -0400

    last small change

--- KopkeHome-FMRS_API/obj/Debug/net6.0/KopkeHome-FMRS_API.AssemblyInfo.cs
+++ KopkeHome-FMRS_API/obj/Debug/net6.0/KopkeHome-FMRS_API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KopkeHome-FMRS_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbfbb15edd86479288f48310d18eb7721584b125")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a46721dca5c7c16ed460807852ba8cb05d08799")]
 [assembly: System.Reflection.AssemblyProductAttribute("KopkeHome-FMRS_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KopkeHome-FMRS_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
